571d8f8cd9cbaac01cb585b6cd4b437f2a39b43a
Faragó Gábor <h355483@stud.u-szeged-hu>
2025-10-07 13:37:08 +0200

    Frontend: dolgozo funkció

--- frontend/bolt-frontend/src/app/pages/dolgozo-kezelo/dolgozo-kezelo.ts
+++ frontend/bolt-frontend/src/app/pages/dolgozo-kezelo/dolgozo-kezelo.ts
@@ -2,17 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { DolgozoService } from '../../shared/services/dolgozoService/dolgozo-service';
 import { Dolgozo } from '../../shared/models/dolgozo.model';
 import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-dolgozo-kezelo',
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatCardModule],
   templateUrl: './dolgozo-kezelo.html',
   styleUrl: './dolgozo-kezelo.scss'
 })
 export class DolgozoKezelo implements OnInit{
 
   dolgozok: DolgozoJelszoval[] = [];
-  displayedColumns: string[] = ['email', 'nev', 'szerepkor']
+  displayedColumns: string[] = ['email', 'nev', 'szerepkor', 'gombok']
   constructor(private dolgozoService: DolgozoService) {}
 
   ngOnInit(): void {
